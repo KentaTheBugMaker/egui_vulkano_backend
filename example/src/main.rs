769657bb401ca8b5a78eeaa660a442139b70ffb5
t18b219k <t18b219k@mail.cc.niigata-u.ac.jp>
2021-03-05 02:24:57 +0900

    reworking example

--- example/src/main.rs
+++ example/src/main.rs
@@ -1,8 +1,14 @@
+use vulkano::buffer::{BufferUsage, CpuAccessibleBuffer};
+use vulkano::command_buffer::{AutoCommandBufferBuilder, DynamicState, SubpassContents};
+use vulkano::descriptor::descriptor_set::PersistentDescriptorSet;
 use vulkano::device::{Device, DeviceExtensions};
-
-use vulkano::image::ImageUsage;
+use vulkano::format::Format;
+use vulkano::framebuffer::{Framebuffer, FramebufferAbstract, RenderPassAbstract, Subpass};
+use vulkano::image::{Dimensions, ImageUsage, ImmutableImage, MipmapsCount, SwapchainImage};
 use vulkano::instance::{Instance, PhysicalDevice};
-
+use vulkano::pipeline::viewport::Viewport;
+use vulkano::pipeline::GraphicsPipeline;
+use vulkano::sampler::{Filter, MipmapMode, Sampler, SamplerAddressMode};
 use vulkano::swapchain;
 use vulkano::swapchain::{
     AcquireError, ColorSpace, FullscreenExclusive, PresentMode, SurfaceTransform, Swapchain,
@@ -14,33 +20,16 @@ use vulkano::sync::{FlushError, GpuFuture};
 use vulkano_win::VkSurfaceBuild;
 use winit::event::{Event, WindowEvent};
 use winit::event_loop::{ControlFlow, EventLoop};
-use winit::window::WindowBuilder;
+use winit::window::{Window, WindowBuilder};
 
-use chrono::Timelike;
-use egui::FontDefinitions;
-use egui_vulkano_backend::ScreenDescriptor;
-use egui_winit_platform::{Platform, PlatformDescriptor};
-use epi::App;
-use std::time::Instant;
+use std::io::Cursor;
 
-enum EguiUserEvent {
-    RequestRedraw,
-}
+use std::sync::Arc;
 
-/// This is the repaint signal type that egui needs for requesting a repaint from another thread.
-/// It sends the custom RequestRedraw event to the winit event loop.
-struct ExampleRepaintSignal(std::sync::Mutex<winit::event_loop::EventLoopProxy<EguiUserEvent>>);
-
-impl epi::RepaintSignal for ExampleRepaintSignal {
-    fn request_repaint(&self) {
-        self.0
-            .lock()
-            .unwrap()
-            .send_event(EguiUserEvent::RequestRedraw)
-            .ok();
-    }
-}
 fn main() {
+    // The start of this example is exactly the same as `triangle`. You should read the
+    // `triangle` example if you haven't done so yet.
+
     let required_extensions = vulkano_win::required_extensions();
     let instance = Instance::new(None, &required_extensions, None).unwrap();
     let physical = PhysicalDevice::enumerate(&instance).next().unwrap();
@@ -49,14 +38,17 @@ fn main() {
         physical.name(),
         physical.ty()
     );
-    let event_loop = EventLoop::with_user_event();
+
+    let event_loop = EventLoop::new();
     let surface = WindowBuilder::new()
         .build_vk_surface(&event_loop, instance.clone())
         .unwrap();
+
     let queue_family = physical
         .queue_families()
         .find(|&q| q.supports_graphics() && surface.is_supported(q).unwrap_or(false))
         .unwrap();
+
     let device_ext = DeviceExtensions {
         khr_swapchain: true,
         ..DeviceExtensions::none()
@@ -67,13 +59,15 @@ fn main() {
         &device_ext,
         [(queue_family, 0.5)].iter().cloned(),
     )
-    .unwrap();
+        .unwrap();
     let queue = queues.next().unwrap();
-    let (mut swapchain, mut images) = {
+
+    let (mut swapchain, images) = {
         let caps = surface.capabilities(physical).unwrap();
         let alpha = caps.supported_composite_alpha.iter().next().unwrap();
         let format = caps.supported_formats[0].0;
         let dimensions: [u32; 2] = surface.window().inner_size().into();
+
         Swapchain::new(
             device.clone(),
             surface.clone(),
@@ -90,36 +84,134 @@ fn main() {
             true,
             ColorSpace::SrgbNonLinear,
         )
-        .unwrap()
+            .unwrap()
     };
-    let repaint_signal = std::sync::Arc::new(ExampleRepaintSignal(std::sync::Mutex::new(
-        event_loop.create_proxy(),
-    )));
-    let mut screen_descriptor = ScreenDescriptor {
-        physical_width: swapchain.dimensions()[0],
-        physical_height: swapchain.dimensions()[1],
-        scale_factor: swapchain.surface().window().scale_factor() as f32,
-    };
-    //init platform
-    let mut platform = Platform::new(PlatformDescriptor {
-        physical_width: swapchain.surface().window().inner_size().width,
-        physical_height: swapchain.surface().window().inner_size().width,
-        scale_factor: swapchain.surface().window().scale_factor(),
-        font_definitions: FontDefinitions::default(),
-        style: Default::default(),
-    });
 
-    //init egui renderpass
-    let mut egui_render_pass = egui_vulkano_backend::EguiVulkanoRenderPass::new(
+    #[derive(Default, Debug, Clone)]
+    struct Vertex {
+        position: [f32; 2],
+    }
+    vulkano::impl_vertex!(Vertex, position);
+
+    let vertex_buffer = CpuAccessibleBuffer::<[Vertex]>::from_iter(
         device.clone(),
-        queue.clone(),
-        swapchain.format(),
+        BufferUsage::all(),
+        false,
+        [
+            Vertex {
+                position: [-0.5, -0.5],
+            },
+            Vertex {
+                position: [-0.5, 0.5],
+            },
+            Vertex {
+                position: [0.5, -0.5],
+            },
+            Vertex {
+                position: [0.5, 0.5],
+            },
+        ]
+            .iter()
+            .cloned(),
+    )
+        .unwrap();
+
+    let vs = vs::Shader::load(device.clone()).unwrap();
+    let fs = fs::Shader::load(device.clone()).unwrap();
+
+    let render_pass = Arc::new(
+        vulkano::single_pass_renderpass!(device.clone(),
+            attachments: {
+                color: {
+                    load: Clear,
+                    store: Store,
+                    format: swapchain.format(),
+                    samples: 1,
+                }
+            },
+            pass: {
+                color: [color],
+                depth_stencil: {}
+            }
+        )
+            .unwrap(),
     );
-    let mut demo_app = egui_demo_lib::WrapApp::default();
-    let mut previous_frame_time = None;
-    let mut previous_frame_end = Some(sync::now(device.clone()).boxed());
-    let mut i = 0;
-    let start_time = Instant::now();
+    let image=vulkano::image::AttachmentImage::new(device.clone(),surface.window().inner_size().into(),swapchain.format()).unwrap();
+
+    let (texture, tex_future) = {
+        let png_bytes = include_bytes!("image_img.png").to_vec();
+        let cursor = Cursor::new(png_bytes);
+        let decoder = png::Decoder::new(cursor);
+        let (info, mut reader) = decoder.read_info().unwrap();
+        let dimensions = Dimensions::Dim2d {
+            width: info.width,
+            height: info.height,
+        };
+        let mut image_data = Vec::new();
+        image_data.resize((info.width * info.height * 4) as usize, 0);
+        reader.next_frame(&mut image_data).unwrap();
+
+        ImmutableImage::from_iter(
+            image_data.iter().cloned(),
+            dimensions,
+            MipmapsCount::One,
+            Format::R8G8B8A8Srgb,
+            queue.clone(),
+        )
+            .unwrap()
+    };
+
+    let sampler = Sampler::new(
+        device.clone(),
+        Filter::Linear,
+        Filter::Linear,
+        MipmapMode::Nearest,
+        SamplerAddressMode::Repeat,
+        SamplerAddressMode::Repeat,
+        SamplerAddressMode::Repeat,
+        0.0,
+        1.0,
+        0.0,
+        0.0,
+    )
+        .unwrap();
+
+    let pipeline = Arc::new(
+        GraphicsPipeline::start()
+            .vertex_input_single_buffer::<Vertex>()
+            .vertex_shader(vs.main_entry_point(), ())
+            .triangle_strip()
+            .viewports_dynamic_scissors_irrelevant(1)
+            .fragment_shader(fs.main_entry_point(), ())
+            .blend_alpha_blending()
+            .render_pass(Subpass::from(render_pass.clone(), 0).unwrap())
+            .build(device.clone())
+            .unwrap(),
+    );
+
+    let layout = pipeline.layout().descriptor_set_layout(0).unwrap();
+    let set = Arc::new(
+        PersistentDescriptorSet::start(layout.clone())
+            .add_sampled_image(texture.clone(), sampler.clone())
+            .unwrap()
+            .build()
+            .unwrap(),
+    );
+
+    let mut dynamic_state = DynamicState {
+        line_width: None,
+        viewports: None,
+        scissors: None,
+        compare_mask: None,
+        write_mask: None,
+        reference: None,
+    };
+    let mut framebuffers =
+        window_size_dependent_setup(&images, render_pass.clone(), &mut dynamic_state);
+
+    let mut recreate_swapchain = false;
+    let mut previous_frame_end = Some(tex_future.boxed());
+
     event_loop.run(move |event, _, control_flow| match event {
         Event::WindowEvent {
             event: WindowEvent::CloseRequested,
@@ -128,74 +220,68 @@ fn main() {
             *control_flow = ControlFlow::Exit;
         }
         Event::WindowEvent {
-            event: WindowEvent::Resized(size),
+            event: WindowEvent::Resized(_),
             ..
         } => {
-            screen_descriptor = ScreenDescriptor {
-                physical_width: size.width,
-                physical_height: size.height,
-                scale_factor: swapchain.surface().window().scale_factor() as f32,
-            };
-            let (new_swapchain, new_images) = match swapchain.recreate_with_dimensions(size.into())
-            {
-                Ok(ok) => ok,
-                Err(SwapchainCreationError::UnsupportedDimensions) => return,
-                Err(err) => panic!("failed to recreate swapchain: {:?}", err),
-            };
-            swapchain = new_swapchain;
-            images = new_images;
+            recreate_swapchain = true;
         }
-        Event::RedrawRequested(..) => {
-            println!("Request redraw {} ", i);
-            i += 1;
-            platform.update_time(start_time.elapsed().as_secs_f64());
-            let (image_num, _, acquire_future) =
+        Event::RedrawEventsCleared => {
+            previous_frame_end.as_mut().unwrap().cleanup_finished();
+
+            if recreate_swapchain {
+                let dimensions: [u32; 2] = surface.window().inner_size().into();
+                let (new_swapchain, new_images) =
+                    match swapchain.recreate_with_dimensions(dimensions) {
+                        Ok(r) => r,
+                        Err(SwapchainCreationError::UnsupportedDimensions) => return,
+                        Err(e) => panic!("Failed to recreate swapchain: {:?}", e),
+                    };
+
+                swapchain = new_swapchain;
+                framebuffers = window_size_dependent_setup(
+                    &new_images,
+                    render_pass.clone(),
+                    &mut dynamic_state,
+                );
+                recreate_swapchain = false;
+            }
+
+            let (image_num, suboptimal, acquire_future) =
                 match swapchain::acquire_next_image(swapchain.clone(), None) {
                     Ok(r) => r,
                     Err(AcquireError::OutOfDate) => {
+                        recreate_swapchain = true;
                         return;
                     }
                     Err(e) => panic!("Failed to acquire next image: {:?}", e),
                 };
 
-            // Begin to draw the UI frame.
-            let egui_start = Instant::now();
-            platform.begin_frame();
-            let mut app_output = epi::backend::AppOutput::default();
-
-            let mut frame = epi::backend::FrameBuilder {
-                info: epi::IntegrationInfo {
-                    web_info: None,
-                    cpu_usage: previous_frame_time,
-                    seconds_since_midnight: Some(seconds_since_midnight()),
-                    native_pixels_per_point: Some(swapchain.surface().window().scale_factor() as _),
-                },
-                tex_allocator: &mut egui_render_pass,
-                output: &mut app_output,
-                repaint_signal: repaint_signal.clone(),
+            if suboptimal {
+                recreate_swapchain = true;
             }
-            .build();
 
-            // Draw the demo application.
-            demo_app.update(&platform.context(), &mut frame);
-
-            // End the UI frame. We could now handle the output and draw the UI with the backend.
-            let (_output, paint_commands) = platform.end_frame();
-            let paint_jobs = platform.context().tessellate(paint_commands);
-
-            //prepare
-            egui_render_pass.upload_egui_texture(&platform.context().texture());
-            egui_render_pass.upload_pending_textures();
-
-            //exec
-            let command_buffer = egui_render_pass.execute(
-                images[image_num].clone(),
-                &paint_jobs,
-                &screen_descriptor,
-            );
-
-            let frame_time = (Instant::now() - egui_start).as_secs_f64() as f32;
-            previous_frame_time = Some(frame_time);
+            let clear_values = vec![[0.0, 0.0, 1.0, 1.0].into()];
+            let mut builder =
+                AutoCommandBufferBuilder::primary_one_time_submit(device.clone(), queue.family())
+                    .unwrap();
+            builder
+                .begin_render_pass(
+                    framebuffers[image_num].clone(),
+                    SubpassContents::Inline,
+                    clear_values,
+                )
+                .unwrap()
+                .draw(
+                    pipeline.clone(),
+                    &dynamic_state,
+                    vertex_buffer.clone(),
+                    set.clone(),
+                    (),
+                )
+                .unwrap()
+                .end_render_pass()
+                .unwrap();
+            let command_buffer = builder.build().unwrap();
 
             let future = previous_frame_end
                 .take()
@@ -211,6 +297,7 @@ fn main() {
                     previous_frame_end = Some(future.boxed());
                 }
                 Err(FlushError::OutOfDate) => {
+                    recreate_swapchain = true;
                     previous_frame_end = Some(sync::now(device.clone()).boxed());
                 }
                 Err(e) => {
@@ -219,15 +306,63 @@ fn main() {
                 }
             }
         }
-        Event::MainEventsCleared | Event::UserEvent(..) => {
-            swapchain.surface().window().request_redraw();
-        }
         _ => (),
     });
 }
 
-/// Time of day as seconds since midnight. Used for clock in demo app.
-pub fn seconds_since_midnight() -> f64 {
-    let time = chrono::Local::now().time();
-    time.num_seconds_from_midnight() as f64 + 1e-9 * (time.nanosecond() as f64)
+/// This method is called once during initialization, then again whenever the window is resized
+fn window_size_dependent_setup(
+    images: &[Arc<SwapchainImage<Window>>],
+    render_pass: Arc<dyn RenderPassAbstract + Send + Sync>,
+    dynamic_state: &mut DynamicState,
+) -> Vec<Arc<dyn FramebufferAbstract + Send + Sync>> {
+    let dimensions = images[0].dimensions();
+
+    let viewport = Viewport {
+        origin: [0.0, 0.0],
+        dimensions: [dimensions[0] as f32, dimensions[1] as f32],
+        depth_range: 0.0..1.0,
+    };
+    dynamic_state.viewports = Some(vec![viewport]);
+
+    images
+        .iter()
+        .map(|image| {
+            Arc::new(
+                Framebuffer::start(render_pass.clone())
+                    .add(image.clone())
+                    .unwrap()
+                    .build()
+                    .unwrap(),
+            ) as Arc<dyn FramebufferAbstract + Send + Sync>
+        })
+        .collect::<Vec<_>>()
+}
+
+mod vs {
+    vulkano_shaders::shader! {
+        ty: "vertex",
+        src: "
+#version 450
+layout(location = 0) in vec2 position;
+layout(location = 0) out vec2 tex_coords;
+void main() {
+    gl_Position = vec4(position, 0.0, 1.0);
+    tex_coords = position + vec2(0.5);
+}"
+    }
+}
+
+mod fs {
+    vulkano_shaders::shader! {
+        ty: "fragment",
+        src: "
+#version 450
+layout(location = 0) in vec2 tex_coords;
+layout(location = 0) out vec4 f_color;
+layout(set = 0, binding = 0) uniform sampler2D tex;
+void main() {
+    f_color = texture(tex, tex_coords);
+}"
+    }
 }
